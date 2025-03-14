4755f4d29744600f24865bf9e4f7ca021e181d2a
Akrom Khasani <akrom.khasani@shortlyst.ai>
2025-03-14 19:50:34 +0700

    Add supabase as extra deps

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ import setuptools
 
 setuptools.setup(
     name="msgbuzz",
-    version="0.1.3",
-    author='Hendra Setiawan',
-    author_email='sihendra@gmail.com',
-    description='Generic message bus abstraction. Supported implementation: RabbitMQ through Pika',
-    long_description=open('README.md').read(),
+    version="0.2.0",
+    author="Hendra Setiawan",
+    author_email="sihendra@gmail.com",
+    description="Generic message bus abstraction. Supported implementation: RabbitMQ through Pika, Supabase Queue",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/sihendra/msgbuzz',
+    url="https://github.com/sihendra/msgbuzz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    install_requires=open('requirements.txt', 'r').readlines(),
-    python_requires='>=3.6'
+    install_requires=open("requirements.txt", "r").readlines(),
+    extras_require={"supabase": ["supabase~=2.13.0"]},
+    python_requires=">=3.9",
 )
