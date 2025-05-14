bdc5cef0348d9d91b046629cc8e353919e533f3c
Akrom Khasani <akrom.khasani@shortlyst.ai>
2025-05-14 12:28:53 +0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="msgbuzz",
-    version="0.2.0",
+    version="0.2.1",
     author="Hendra Setiawan",
     author_email="sihendra@gmail.com",
     description="Generic message bus abstraction. Supported implementation: RabbitMQ through Pika, Supabase Queue",
